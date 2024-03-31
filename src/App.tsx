a7b3ce780957e4bdb4839552d8ba0e5aab875dc4
Thomas Bach <thomasbach38@gmail.com>
2024-03-31 13:07:39 +0200

    fix: responsive home button

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Router from "./components/Router";
 function App() {
   return (
     <>
-      <Header />
-      <main className="sm:mt-32 mt-16">
-        <Router />
-      </main>
-      <Footer />
+      <div className="px-8 sm:px-16 bg-white border-x-[1px] border-b-[1px] border-slate-100 border-solid">
+        <Header />
+        <main className="sm:mt-32 mt-16">
+          <Router />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
